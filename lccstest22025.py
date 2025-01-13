d08544726b8a7813cc22bb04bad858b7b51a395d
Aishah <154215253+asoliman02@users.noreply.github.com>
2025-01-13 11:32:19 +0000

    Add files via upload

---
+++ lccstest22025.py
@@ -0,0 +1,19 @@
+import csv
+import pandas
+import statistics
+import matplotlib.pyplot as plt
+import flask
+
+df = pandas.read_csv('report2022202312.csv')
+
+years = [2023,2023,2023,2023,2023,2023,2023,2023,2023,2023,2023,2023,2023,2023,2023,2023,2023,2023,2023,2023,2022,2022,2022,2022,2022,2022,2022,2022,2022,2022]
+
+print("Years of data taken:", years)
+unique_values = []
+for value in years:
+    if value not in unique_values:
+        unique_values.append(value)
+        
+
+Incoming_s_xual = 
+
