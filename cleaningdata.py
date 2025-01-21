7b48baa012712425c2b9387af4310e9059489a06
Aishah <154215253+asoliman02@users.noreply.github.com>
2025-01-21 09:14:39 +0000

    Add files via upload

--- cleaningdata.py
+++ cleaningdata.py
@@ -2,6 +2,7 @@ import csv
 import pandas
 import statistics
 import flask
+import plotly.express as px
 # Input and output file names
 dirty_report = 'report2022202312.csv'
 cleaned_report = 'cleanedreport.csv'
@@ -14,11 +15,13 @@ def contains_invalid_characters(row):
             return True
     return False
 
+df.drop("AREA_OF_LAW")
+
 # Open the input file and process the rows
 with open(dirty_report, 'r') as infile, open(cleaned_report, 'w', newline='') as outfile:
     csv_reader = csv.reader(infile)
     csv_writer = csv.writer(outfile)
-    df.drop("AREAS_OF_LAW")
+    
  
     # Write the header to the output file
     header = next(csv_reader)
@@ -32,9 +35,15 @@ with open(dirty_report, 'r') as infile, open(cleaned_report, 'w', newline='') as
 print(f"Cleaned data has been written to {cleaned_report}")
 
 
-app = Flask(__name__)
-@app.route("home")
+
+app = Flask(__name__)  # Create a Flask app instance
+
+@app.route("home")  # Define a route for the root URL
 def home():
-    return "This is the Home Page"
+    return "This is the home page."  # Response to the browser
+
+if __name__ == "__main__":
+    app.run(debug=True)  # Run the app in debug mode
+
 
 
